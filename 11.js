9b0685b24e9b3b2c71564e8b23137c18c9f108e7
ketrin_stecenko <you@example.comstecenko.ketrin@gmail.com>
2020-01-03 22:20:52 +0200

    add task 11

---
+++ 11.js
@@ -0,0 +1,7 @@
+let elem = document.getElementById('myunique')
+
+elem.onclick = (event) => {
+    if(elem.classList.contains('www')) {
+        elem.removeAttribute('class')
+    } else elem.classList.add('www')
+} 
